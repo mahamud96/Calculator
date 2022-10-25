e992239e2cd94954af1ea730e1ce2da0de62063b
Mahamud Ahmed <mahamud1996@outlook.com>
2022-10-25 10:42:59 +0100

    Add data attribute to HTML and add query selectors on JS to capture all data attributes

---
+++ script.js
@@ -0,0 +1,5 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const deleteButton = document.querySelector('[data-delete]')
+const equalsbutton = document.querySelector('[data-equals]')
